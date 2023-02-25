52798ce8428888283fa7dbf7e3c5eb49b7faf22f
Mario Capitbrok <mario@capitbrok.com>
2023-02-25 15:57:58 -0600

    Adding time travel.

---
+++ 04_tic-tac-toe/.history/src/components/Game_20230225154003.jsx
@@ -0,0 +1,20 @@
+import Board from './Board'
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>
+          {
+            {
+              /* TODO */
+            }
+          }
+        </ol>
+      </div>
+    </div>
+  )
+}
